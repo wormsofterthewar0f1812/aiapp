5c8ea9477f3b875805adb22c5ba49713b99f5ffe
wormsofterthewar0f1812 <zeea3906@psdblogs.ca>
2026-01-16 09:11:01 -0700

    e
    e

---
+++ sw.js
@@ -0,0 +1,24 @@
+﻿const CACHE_NAME = 'editor-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+
+// Install event: Caches the app shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+
+// Fetch event: Serves files from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
