872a0f97eae546e0dff9ae0a92f71085ba31c431
wormsofterthewar0f1812 <zeea3906@psdblogs.ca>
2026-01-16 09:21:15 -0700

    Update sw.js
    fixed?

--- sw.js
+++ sw.js
@@ -1,26 +1,20 @@
-﻿const CACHE_NAME = 'editor-v1';
+const CACHE_NAME = 'aiapp-v1';
 const ASSETS = [
   '/aiapp/',
   '/aiapp/index.html',
   '/aiapp/manifest.json'
 ];
 
-
-
-// Install event: Caches the app shell
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-
-// Fetch event: Serves files from cache if offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
-
 });
